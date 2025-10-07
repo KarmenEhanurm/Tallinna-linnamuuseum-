1561065c4892cd7d9da2a2439035a3579a9e7b2e
karmenott <karmen.ott@ut.ee>
2025-10-07 19:07:39 +0300

    Create CoinService class and  implement a demo generateNewCoin() function

---
+++ src/service/coin-service.ts
@@ -0,0 +1,20 @@
+import { Coin } from "../data/entity/coin"
+
+/**
+ * Coin service provides
+ */
+export class CoinService {
+    /**
+     * Generates new coin for the coin flipper game
+     * and returns its value
+     */
+    public generateNewCoin() : Coin {
+        return {
+            title: "tukat",
+            date: "1990 - 2000",
+            id: "1201021",
+            headImageResource: require("./assets/images/demo/museaal-1201021-head.webp"),
+            tailsImageResource: require("./assets/images/demo/museaal-1201021-head.webp")
+        }
+    }
+};
