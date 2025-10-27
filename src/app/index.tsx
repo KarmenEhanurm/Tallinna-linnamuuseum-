d71f5dd6e13a6e3a11d4e251fe9f233bb3f03052
karmenott <karmen.ott@ut.ee>
2025-10-27 13:27:47 +0200

    Add some text to index.tsx

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,24 @@
+import { HeaderTitle } from "@react-navigation/elements";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={{
+      flex: 1
+    }}>
+      <HeaderTitle style={{textAlign: "center", marginTop: 10, fontSize: 32}}>Tere tulemast 👋</HeaderTitle>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>
+          Tere tulemast Tallinna linnamuuseumi mündirakendusse!
+          Siin saad digitaalselt koguda kõikvõimalikke ajaloolisi münte ning neid visata 🎉
+        </Text>
+      </View>
     </View>
   );
 }
