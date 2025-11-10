f4524b76b05b3e062dac203d41221e35e56280aa
karmenott <karmen.ott@ut.ee>
2025-11-10 10:39:44 +0200

    Modify firebase config to read key values from environment variables

---
+++ app.config.js
@@ -0,0 +1,15 @@
+export default {
+    expo: {
+        extra: {
+            firebase: {
+                apiKey: process.env.EXPO_API_KEY,
+                authDomain: process.env.EXPO_AUTH_DOMAIN,
+                projectId: process.env.EXPO_PROJECT_ID,
+                storageBucket: process.env.EXPO_STORAGE_BUCKET,
+                messagingSenderId: process.env.EXPO_MESSAGING_SENDER_ID,
+                appId: process.env.EXPO_APP_ID,
+                measurementId: process.env.EXPO_MEASUREMENT_ID
+            }
+        }
+    }
+}
--- src/config.ts
+++ src/config.ts
@@ -1,17 +1,16 @@
 import { FirebaseOptions, initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
-import Config from 'react-native-config';
+import Constants from 'expo-constants';
+//import Config from 'react-native-config';
 
 // Configuration values are taken from the root .env file
-const firebaseConfig: FirebaseOptions = {
-    apiKey: Config.API_KEY,
-    authDomain: Config.AUTH_DOMAIN,
-    projectId: Config.PROJECT_ID,
-    storageBucket: Config.STORAGE_BUCKET,
-    messagingSenderId: Config.MESSAGING_SENDER_ID,
-    appId: Config.APP_ID,
-    measurementId: Config.MEASUREMENT_ID
+export const firebaseConfig: FirebaseOptions = {
+    apiKey: Constants.expoConfig?.extra?.firebase.apiKey,
+    authDomain: Constants.expoConfig?.extra?.firebase.authDomain,
+    projectId: Constants.expoConfig?.extra?.firebase.projectId,
+    storageBucket: Constants.expoConfig?.extra?.firebase.storageBucket,
+    messagingSenderId: Constants.expoConfig?.extra?.firebase.messagingSenderId,
+    appId: Constants.expoConfig?.extra?.firebase.appId,
+    measurementId: Constants.expoConfig?.extra?.firebase.measurementId
 };
 
 export const app = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(app);
